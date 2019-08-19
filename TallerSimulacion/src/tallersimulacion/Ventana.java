7019ed1a43ae75ef40e1a202382881e6566cb228
RudyMontenegro <rudymontenegro4@gmail.com>
2019-08-19 16:42:43 -0400

    Añadido Menu

--- TallerSimulacion/src/tallersimulacion/Ventana.java
+++ TallerSimulacion/src/tallersimulacion/Ventana.java
@@ -288,6 +288,7 @@ public class Ventana extends javax.swing.JFrame {
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
+        Menu m1 = new Menu();
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
